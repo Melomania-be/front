36fc85e5fab31c17fbcc613add027afc0c57c7d1
Califano9577 <amaury.giot@student.junia.com>
2024-07-05 17:17:21 +0200

    nouvelle page fini

--- src/routes/api/contacts/validations/+server.ts
+++ src/routes/api/contacts/validations/+server.ts
@@ -2,8 +2,14 @@ import { getToken } from '$lib/server/authentification';
 import { type RequestHandler } from '@sveltejs/kit';
 import { BACKEND_API_HOST, BACKEND_API_PORT } from '$env/static/private';
 
-export const GET: RequestHandler = async ({ cookies, fetch }) => {
-	const res = await fetch(`http://${BACKEND_API_HOST}:${BACKEND_API_PORT}/contact/`, {
+export const GET: RequestHandler = async ({ cookies, url, fetch }) => {
+	const page = url.searchParams.get('page');
+	const limit = url.searchParams.get('limit');
+	const filter = url.searchParams.get('filter');
+	const orderBy = url.searchParams.get('orderBy');
+	const order = url.searchParams.get('order');
+
+	const res = await fetch(`http://${BACKEND_API_HOST}:${BACKEND_API_PORT}/contact/validation?limit=${limit}&page=${page}&filter=${filter}&orderBy=${orderBy}&order=${order}`, {
 		method: 'GET',
 		headers: {
 			authorization: `${await getToken(cookies)}`
--- src/routes/api/recommended/+server.ts
+++ src/routes/api/recommended/+server.ts
@@ -16,3 +16,25 @@ export const POST: RequestHandler = async ({ cookies, fetch, request }) => {
 	});
 	return res;
 };
+
+export const GET: RequestHandler = async ({ cookies, url, fetch }) => {
+	const page = url.searchParams.get('page');
+	const limit = url.searchParams.get('limit');
+	const filter = url.searchParams.get('filter');
+	const orderBy = url.searchParams.get('orderBy');
+	const order = url.searchParams.get('order');
+
+	const res = await fetch(
+		`http://${BACKEND_API_HOST}:${BACKEND_API_PORT}/recommend_someone?limit=${limit}&page=${page}&filter=${filter}&orderBy=${orderBy}&order=${order}`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application',
+				authorization: `${await getToken(cookies)}`
+			}
+		}
+	);
+
+	console.log(res);
+	return res;
+};
