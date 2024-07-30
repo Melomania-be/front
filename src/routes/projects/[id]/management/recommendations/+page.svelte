611de606dd2127553c283d6a4c7b7c809ff788fc
Califano9577 <amaury.giot@student.junia.com>
2024-07-30 14:50:48 +0200

    ajout bouton update liste

--- src/routes/projects/[id]/management/recommendations/+page.svelte
+++ src/routes/projects/[id]/management/recommendations/+page.svelte
@@ -468,6 +468,8 @@
 
 
 		<div>
+			<button on:click={fetchData} type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Update the list</button>
+
 			{#if dataHolderContact}
 				<div class="grid grid-cols-1 w-full mt-2">
 					<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
