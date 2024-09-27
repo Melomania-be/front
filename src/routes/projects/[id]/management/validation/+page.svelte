38bd826e3777dc010c1ebfbc89aca122802c5013
Eremith <loic.gdl2001@gmail.com>
2024-09-27 17:27:34 +0200

    improve style of management validation + responsive

--- src/routes/projects/[id]/management/validation/+page.svelte
+++ src/routes/projects/[id]/management/validation/+page.svelte
@@ -1,208 +1,197 @@
 <script lang="ts">
-	import AttendancePicker from '$lib/components/participant/AttendancePicker.svelte';
-	import RegistrationForm from '$lib/components/registration/RegistrationForm.svelte';
-	import type { Participant } from '$lib/types/Participant.js';
-	import { onMount } from 'svelte';
+    import AttendancePicker from '$lib/components/participant/AttendancePicker.svelte';
+    import RegistrationForm from '$lib/components/registration/RegistrationForm.svelte';
+    import type { Participant } from '$lib/types/Participant.js';
+    import { onMount } from 'svelte';
 
-	export let data;
-	let participants: Array<Participant>;
-	let currentParticipant: Participant | null;
+    export let data;
+    let participants: Array<Participant>;
+    let currentParticipant: Participant | null;
 
-	onMount(async () => {
-		const responseParticipants = await fetch(`/api/projects/${data.id}/management/validation`);
+    onMount(async () => {
+        const responseParticipants = await fetch(`/api/projects/${data.id}/management/validation`);
 
-		if (responseParticipants.ok) {
-			participants = await responseParticipants.json();
-		}
-	});
+        if (responseParticipants.ok) {
+            participants = await responseParticipants.json();
+        }
+    });
 
-	async function deleteParticipant() {
-		const response = await fetch(
-			`/api/projects/${data.id}/management/participants/${currentParticipant!.id}`,
-			{
-				method: 'DELETE',
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}
-		);
+    async function deleteParticipant() {
+        const response = await fetch(
+            `/api/projects/${data.id}/management/participants/${currentParticipant!.id}`,
+            {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
 
-		if (response.ok) {
-			participants = participants.filter(
-				(participant) => participant.id !== currentParticipant!.id
-			);
-			currentParticipant = null;
-		}
-	}
+        if (response.ok) {
+            participants = participants.filter(
+                (participant) => participant.id !== currentParticipant!.id
+            );
+            currentParticipant = null;
+        }
+    }
 
-	async function validateParticipant() {
-		const responseEmail = await fetch(`/api/mailing/sendParticipationValidationNotifications`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ projectId: data.id, contactId: currentParticipant!.contact.id })
-		});
+    async function validateParticipant() {
+        const responseEmail = await fetch(`/api/mailing/sendParticipationValidationNotifications`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ projectId: data.id, contactId: currentParticipant!.contact.id })
+        });
 
-		if (!responseEmail.ok) {
-			console.error('Failed to send email');
-		}
-		
-		const response = await fetch(`/api/projects/${data.id}/management/validation`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ id: currentParticipant!.id })
-		});
+        if (!responseEmail.ok) {
+            console.error('Failed to send email');
+        }
+        
+        const response = await fetch(`/api/projects/${data.id}/management/validation`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id: currentParticipant!.id })
+        });
 
-		if (response.ok) {
-			participants = participants.filter(
-				(participant) => participant.id !== currentParticipant!.id
-			);
-			currentParticipant = null;
-		}
-	}
+        if (response.ok) {
+            participants = participants.filter(
+                (participant) => participant.id !== currentParticipant!.id
+            );
+            currentParticipant = null;
+        }
+    }
 </script>
 
-<div
-	class="m-1 relative max-w-xxl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
->
-	<div class="grid grid-cols-2">
-		{#if participants && participants.length > 0}
-			<div>
-				<h1 class="text-2xl">
-					You need to validate the registration of {participants.length} person(s) :
-				</h1>
-				<table>
-					<thead>
-						<tr>
-							<th>Firstname</th>
-							<th>Lastname</th>
-							<th>Section</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each participants as participant}
-							<tr class="hover:border-blue-700 border cursor-pointer">
-								<button on:click={() => (currentParticipant = participant)}>
-									<td>{participant.contact.firstName}</td>
-									<td>{participant.contact.lastName}</td>
-									<td>{participant.section.name}</td>
-									<td>
-										<span class="icon-[formkit--arrowright] hover:text-black"></span>
-									</td>
-								</button>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</div>
-		{:else}
-			<h1 class="text-2xl">There is no participant to validate</h1>
-		{/if}
-		{#if currentParticipant}
-			<div class="relative border m-1 rounded">
-				<div class={currentParticipant.contact.validated ? '' : 'opacity-10 pointer-events-none'}>
-					<h1 class="text-2xl w-full text-center">Participant request :</h1>
-					<div>
-						<h2 class="text-2xl">Contact :</h2>
-						<div class="border">
-							<div>
-								<span class="text-lg">Name :</span>
-								<span
-									>{currentParticipant.contact.firstName}
-									{currentParticipant.contact.lastName}</span
-								>
-							</div>
-							<div>
-								<span class="text-lg">Email :</span>
-								<a href="mailto:{currentParticipant.contact.email}"
-									>{currentParticipant.contact.email}</a
-								>
-							</div>
-							<div>
-								<span class="text-lg">Phone :</span>
-								<span>{currentParticipant.contact.phone}</span>
-							</div>
-							<div>
-								<span class="text-lg">Messenger :</span>
-								<span>{currentParticipant.contact.messenger}</span>
-							</div>
-							<div>
-								<span class="text-lg">Comments :</span>
-								<span>{currentParticipant.contact.comments ?? 'no comments'}</span>
-							</div>
-						</div>
+<div class="m-4 p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {#if participants && participants.length > 0}
+            <div>
+                <h1 class="text-2xl mb-4">You need to validate the registration of {participants.length} person(s):</h1>
+                <table class="min-w-full bg-white dark:bg-gray-800">
+                    <thead>
+                        <tr>
+                            <th class="py-2 px-4 border-b">Firstname</th>
+                            <th class="py-2 px-4 border-b">Lastname</th>
+                            <th class="py-2 px-4 border-b">Section</th>
+                            <th class="py-2 px-4 border-b"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {#each participants as participant}
+                            <tr class="hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer">
+                                <td class="py-2 px-4 border-b" on:click={() => (currentParticipant = participant)}>{participant.contact.firstName}</td>
+                                <td class="py-2 px-4 border-b" on:click={() => (currentParticipant = participant)}>{participant.contact.lastName}</td>
+                                <td class="py-2 px-4 border-b" on:click={() => (currentParticipant = participant)}>{participant.section.name}</td>
+                                <td class="py-2 px-4 border-b text-right">
+                                    <button class="text-blue-500 hover:text-blue-700" on:click={() => (currentParticipant = participant)}>
+                                        <span class="icon-[formkit--arrowright]"></span>
+                                    </button>
+                                </td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+            </div>
+        {:else}
+            <h1 class="text-2xl">There is no participant to validate</h1>
+        {/if}
+        {#if currentParticipant}
+            <div class="relative border p-4 rounded-lg bg-gray-50 dark:bg-gray-900">
+                <div class={currentParticipant.contact.validated ? '' : 'opacity-50 pointer-events-none'}>
+                    <h1 class="text-2xl text-center mb-4">Participant request:</h1>
+                    <div class="mb-4">
+                        <h2 class="text-xl mb-2">Contact:</h2>
+                        <div class="border p-2 rounded-lg bg-white dark:bg-gray-800">
+                            <div class="mb-2">
+                                <span class="font-semibold">Name:</span> {currentParticipant.contact.firstName} {currentParticipant.contact.lastName}
+                            </div>
+                            <div class="mb-2">
+                                <span class="font-semibold">Email:</span> <a href="mailto:{currentParticipant.contact.email}" class="text-blue-500">{currentParticipant.contact.email}</a>
+                            </div>
+                            <div class="mb-2">
+                                <span class="font-semibold">Phone:</span> {currentParticipant.contact.phone}
+                            </div>
+                            <div class="mb-2">
+                                <span class="font-semibold">Messenger:</span> {currentParticipant.contact.messenger}
+                            </div>
+                            <div class="mb-2">
+                                <span class="font-semibold">Comments:</span> {currentParticipant.contact.comments ?? 'no comments'}
+                            </div>
+                        </div>
+                    </div>
 
-						<h2 class="text-2xl">Registration form :</h2>
-						<div class="border">
-							<div>
-								<span class="text-lg">Section :</span>
-								<span>{currentParticipant.section.name}</span>
-							</div>
-							<div>
-								<span class="text-lg">Custom form :</span>
-								{#if currentParticipant.answers.length > 0}
-									{#each currentParticipant.answers as answer}
-										{#if answer.form}
-											<RegistrationForm forms={[]} bind:answer disabled />
-										{/if}
-									{/each}
-								{:else}
-									<span>No custom form</span>
-								{/if}
-							</div>
-							<div>
-								<h3 class="text-xl">Rehearsals</h3>
-								<AttendancePicker
-									concertsOrRehearsals={currentParticipant.rehearsals}
-									type="rehearsal"
-									participants={[currentParticipant]}
-									disabled
-								/>
-								<h3 class="text-xl">Concerts</h3>
-								<AttendancePicker
-									concertsOrRehearsals={currentParticipant.concerts}
-									type="concert"
-									participants={[currentParticipant]}
-									disabled
-								/>
-							</div>
-						</div>
-					</div>
-					<div>
-						<button
-							class="bg-blue-700 text-white rounded p-1 m-1"
-							on:click={() => {
-								validateParticipant();
-							}}
-						>
-							Validate and send confirmation email
-						</button>
-						<button
-							class="bg-red-700 text-white rounded p-1 m-1"
-							on:click={() => {
-								deleteParticipant();
-							}}
-						>
-							Refuse and delete participant
-						</button>
-					</div>
-				</div>
+                    <div class="mb-4">
+                        <h2 class="text-xl mb-2">Registration form:</h2>
+                        <div class="border p-2 rounded-lg bg-white dark:bg-gray-800">
+                            <div class="mb-2">
+                                <span class="font-semibold">Section:</span> {currentParticipant.section.name}
+                            </div>
+                            <div class="mb-2">
+                                <span class="font-semibold">Custom form:</span>
+                                {#if currentParticipant.answers.length > 0}
+                                    {#each currentParticipant.answers as answer}
+                                        {#if answer.form}
+                                            <RegistrationForm forms={[]} bind:answer disabled />
+                                        {/if}
+                                    {/each}
+                                {:else}
+                                    <span>No custom form</span>
+                                {/if}
+                            </div>
+                            <div class="mb-2">
+                                <h3 class="text-lg">Rehearsals</h3>
+                                <AttendancePicker
+                                    concertsOrRehearsals={currentParticipant.rehearsals}
+                                    type="rehearsal"
+                                    participants={[currentParticipant]}
+                                    disabled
+                                />
+                            </div>
+                            <div class="mb-2">
+                                <h3 class="text-lg">Concerts</h3>
+                                <AttendancePicker
+                                    concertsOrRehearsals={currentParticipant.concerts}
+                                    type="concert"
+                                    participants={[currentParticipant]}
+                                    disabled
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-between">
+                        <button
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            on:click={() => {
+                                validateParticipant();
+                            }}
+                        >
+                            Validate and send confirmation email
+                        </button>
+                        <button
+                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                            on:click={() => {
+                                deleteParticipant();
+                            }}
+                        >
+                            Refuse and delete participant
+                        </button>
+                    </div>
+                </div>
 
-				{#if !currentParticipant.contact.validated}
-					<div class="absolute top-0 pointer-events-auto">
-						<h1 class="text-2xl">You need to validate this persons first !</h1>
-						<a href="/contacts/validation" class="text-blue-700">Contact validation page</a>
-					</div>
-				{/if}
-			</div>
-		{:else}
-			<div class="border m-1 rounded">
-				<h1 class="text-2xl">No participant selected</h1>
-			</div>
-		{/if}
-	</div>
+                {#if !currentParticipant.contact.validated}
+                    <div class="absolute top-0 left-0 right-0 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-lg">
+                        <h1 class="text-2xl mb-2">You need to validate this person first!</h1>
+                        <a href="/contacts/validation" class="text-blue-500">Contact validation page</a>
+                    </div>
+                {/if}
+            </div>
+        {:else}
+            <div class="border p-4 rounded-lg bg-gray-50 dark:bg-gray-900">
+                <h1 class="text-2xl">No participant selected</h1>
+            </div>
+        {/if}
+    </div>
 </div>
