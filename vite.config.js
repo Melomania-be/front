9ab7d946fe3e5db2aed2ecb436be45ed1a6dcddf
Collins <collins.lemogo-kamnen@student.junia.com>
2025-07-27 04:32:54 +0200

    creation du bouton PDF attendance (#142)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+// vite.config.ts
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['jspdf', 'jspdf-autotable']
+	},
+	build: {
+		rollupOptions: {
+			external: [],
+			output: {
+				globals: {}
+			}
+		}
+	}
 });
