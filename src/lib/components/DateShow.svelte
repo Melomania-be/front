828b9895de7836a845da6a7f4e4eff98319f56c1
N-nketsiah <127321079+N-nketsiah@users.noreply.github.com>
2026-05-29 15:13:18 +0500

    Fix date display for both rehearsals and same-day concerts - Umair

--- src/lib/components/DateShow.svelte
+++ src/lib/components/DateShow.svelte
@@ -56,7 +56,7 @@
 			{usableEndTime.toLocaleTimeString(undefined, timeOption)}
 		{:else if withDate}
 			{usableStartTime.toLocaleDateString(undefined, dateOptions)}
-			{#if endTime && !isRehearsal}
+			{#if endTime && !isRehearsal && usableStartTime.toDateString() !== usableEndTime.toDateString()}
 				{' to '}
 				{usableEndTime.toLocaleDateString(undefined, dateOptions)}
 			{/if}
