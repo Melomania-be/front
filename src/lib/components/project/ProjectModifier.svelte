060b5f25c6e8467ed71b20b68eaa92fdedf46fbf
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2026-06-18 21:15:45 +0200

    Updated to accomodate Android Webview

--- src/lib/components/project/ProjectModifier.svelte
+++ src/lib/components/project/ProjectModifier.svelte
@@ -137,12 +137,19 @@
 			},
 			animation: 200,
 			sort: false,
+			forceFallback: true,
+			fallbackTolerance: 3,
+			delay: 50,
+			delayOnTouchOnly: true,
+			touchStartThreshold: 5,
 			onAdd: (evt: any) => {
-				const item = selectedPieces[evt.oldIndex as number];
-				allPieces.splice(evt.newIndex as number, 0, item);
-			},
-			onRemove: (evt: any) => {
-				allPieces.splice(evt.oldIndex as number, 1);
+				const id = parseInt(evt.item.getAttribute('data-id') || '0');
+				const item = pieces.find(p => p.id === id);
+				if (item) {
+					allPieces.splice(evt.newIndex as number, 0, item);
+					allPieces = [...allPieces];
+					selectedPieces = selectedPieces.filter(p => p.id !== id);
+				}
 			}
 		});
 
@@ -153,17 +160,28 @@
 				pull: true
 			},
 			animation: 200,
+			forceFallback: true,
+			fallbackTolerance: 3,
+			delay: 50,
+			delayOnTouchOnly: true,
+			touchStartThreshold: 5,
 			onAdd: (evt: any) => {
-				const item = allPieces[evt.oldIndex as number];
-				selectedPieces.splice(evt.newIndex as number, 0, item);
-			},
-			onRemove: (evt: any) => {
-				selectedPieces.splice(evt.oldIndex as number, 1);
+				const id = parseInt(evt.item.getAttribute('data-id') || '0');
+				const item = pieces.find(p => p.id === id);
+				if (item) {
+					selectedPieces.splice(evt.newIndex as number, 0, item);
+					selectedPieces = [...selectedPieces];
+					allPieces = allPieces.filter(p => p.id !== id);
+				}
 			},
 			onUpdate: (evt: any) => {
-				const item = selectedPieces[evt.oldIndex as number];
-				selectedPieces.splice(evt.oldIndex as number, 1);
-				selectedPieces.splice(evt.newIndex as number, 0, item);
+				const id = parseInt(evt.item.getAttribute('data-id') || '0');
+				const item = selectedPieces.find(p => p.id === id);
+				if (item) {
+					const temp = selectedPieces.filter(p => p.id !== id);
+					temp.splice(evt.newIndex as number, 0, item);
+					selectedPieces = temp;
+				}
 			}
 		});
 	}
