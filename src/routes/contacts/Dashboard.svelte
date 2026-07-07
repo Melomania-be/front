b1f53cc182f0fb222bb3d21f8083cc466bfad23d
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2026-07-07 09:57:15 +0200

    updates for mobile

--- src/routes/contacts/Dashboard.svelte
+++ src/routes/contacts/Dashboard.svelte
@@ -11,106 +11,108 @@
 	<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 		<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-800 dark:text-gray-400">
 			<tr>
-				<th scope="col" class="px-4 py-3">Link</th>
-				<th scope="col" class="px-4 py-3">Infos</th>
-				<th scope="col" class="px-2 py-3 sm:px-4">Action</th>
+				<th scope="col" class="px-2 sm:px-4 py-3 whitespace-nowrap">Link</th>
+				<th scope="col" class="px-2 sm:px-4 py-3 whitespace-nowrap">Infos</th>
+				<th scope="col" class="px-2 sm:px-4 py-3 whitespace-nowrap">Action</th>
 			</tr>
 		</thead>
 		<tbody>
 			<tr class="border-b border-gray-200 dark:border-gray-700">
-				<th scope="row" class="px-4 py-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+				<th scope="row" class="px-2 sm:px-4 py-3 font-medium text-blue-600 dark:text-blue-500 hover:underline">
 					<a
 						href="/contacts/search"
-						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-3 sm:px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 					>
-						All contact
+						<span class="hidden sm:inline">All contact</span>
+						<span class="inline sm:hidden">All</span>
 					</a>
 				</th>
-				<td class="px-4 py-4">{data.numberContact} contacts</td>
-				<th class="px-2 py-4 sm:px-4">
+				<td class="px-2 sm:px-4 py-3 whitespace-nowrap">{data.numberContact} contacts</td>
+				<th>
 					<a
 						href="/contacts/create"
-						class="inline-flex whitespace-nowrap text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-[11px] sm:text-sm px-2.5 sm:px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 					>
-						Create new contact
+						<span class="hidden sm:inline">Create new contact</span>
+						<span class="inline sm:hidden">Create</span>
 					</a>
 				</th>
 			</tr>
 			<tr class="border-b border-gray-200 dark:border-gray-700">
-	<th scope="row" class="px-4 py-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
-		<a
-			href="/contractors"
-			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-3 sm:px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-		>
-			Contractor contacts
-		</a>
-	</th>
-
-	<td class="px-4 py-4">
-		External contacts database
-	</td>
-
-	<th>
-		<a
-			href="/contractors/create"
-			class="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-xs sm:text-sm px-3 sm:px-5 py-2"
-		>
-			Create contractor
-		</a>
-	</th>
-</tr>
+				<th scope="row" class="px-2 sm:px-4 py-3 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+					<a
+						href="/contractors"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+					>
+						<span class="hidden sm:inline">Contractor contacts</span>
+						<span class="inline sm:hidden">Contractors</span>
+					</a>
+				</th>
+				<td class="px-2 sm:px-4 py-3 whitespace-nowrap">External contacts database</td>
+				<th>
+					<a
+						href="/contractors/create"
+						class="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap"
+					>
+						<span class="hidden sm:inline">Create contractor</span>
+						<span class="inline sm:hidden">Create</span>
+					</a>
+				</th>
+			</tr>
 			<tr class="border-b border-gray-200 dark:border-gray-700">
-				<th scope="row" class="px-4 py-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+				<th scope="row" class="px-2 sm:px-4 py-3 font-medium text-blue-600 dark:text-blue-500 hover:underline">
 					<a
 						href="/contacts/lists"
-						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-3 sm:px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 					>
-						All lists
+						<span class="hidden sm:inline">All lists</span>
+						<span class="inline sm:hidden">All</span>
 					</a>
 				</th>
-				<td class="px-4 py-4">{data.numberListe} Lists</td>
-				<th class="px-2 py-4 sm:px-4">
+				<td class="px-2 sm:px-4 py-3 whitespace-nowrap">{data.numberListe} Lists</td>
+				<th>
 					<a
 						href="/contacts/lists/create"
-						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-3 sm:px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 					>
-						Create new list
+						<span class="hidden sm:inline">Create new list</span>
+						<span class="inline sm:hidden">Create</span>
 					</a>
 				</th>
 			</tr>
 			<tr class="border-b border-gray-200 dark:border-gray-700">
-				<th scope="row" class="px-4 py-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+				<th scope="row" class="px-2 sm:px-4 py-3 font-medium text-blue-600 dark:text-blue-500 hover:underline">
 					<a
 						href="/contacts/validation"
-						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-3 sm:px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 					>
 						To validate
 					</a>
 				</th>
-				<td class="px-4 py-4">
+				<td class="px-2 sm:px-4 py-3 whitespace-nowrap">
 					<span class="inline-flex items-center justify-center w-6 h-6 text-xs font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
 						{data.numberValidationContact}
 					</span>
 					<span class="ml-2">to validate</span>
 				</td>
-				<td class="px-2 py-4 sm:px-4"></td>
+				<td></td>
 			</tr>
 			<tr class="border-b border-gray-200 dark:border-gray-700">
-				<th scope="row" class="px-4 py-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+				<th scope="row" class="px-2 sm:px-4 py-3 font-medium text-blue-600 dark:text-blue-500 hover:underline">
 					<a
 						href="/contacts/recommended"
-						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-3 sm:px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-1.5 sm:py-2 whitespace-nowrap dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 					>
 						Recommended
 					</a>
 				</th>
-				<td class="px-4 py-4">
+				<td class="px-2 sm:px-4 py-3 whitespace-nowrap">
 					<span class="inline-flex items-center justify-center w-6 h-6 text-xs font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
 						{data.numberRecommendedContact}
 					</span>
 					<span class="ml-2">recommended</span>
 				</td>
-				<td class="px-2 py-4 sm:px-4"></td>
+				<td></td>
 			</tr>
 		</tbody>
 	</table>
