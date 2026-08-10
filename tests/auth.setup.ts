8ac5eb7a65dc8a6e8620e4d743f3e40b9520ea2c
smbv <brice-verges.sandjong-mbiada@student.junia.com>
2026-08-10 10:05:39 +0200

    fix: force wait for svelte hydration on github actions

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,7 +1,6 @@
 import { test as setup } from '@playwright/test'
 
 setup('authenticate', async ({ page }) => {
-	// 1. On intercepte l'appel exact fait par ton frontend
 	await page.route('**/api/sign_in', async route => {
 		await route.fulfill({
 			status: 200,
@@ -10,21 +9,17 @@ setup('authenticate', async ({ page }) => {
 		});
 	});
 
-	// 2. On charge la page de login
 	await page.goto('http://localhost:5173/login')
 
-	// 3. On remplit le formulaire
-	await page.locator('#email').click()
-	await page.locator('#email').pressSequentially('admin@admin.admin')
-	await page.locator('#password').click()
-	await page.locator('#password').pressSequentially('admin')
+	// La pause magique pour compenser la lenteur de GitHub Actions
+	await page.waitForTimeout(3000)
 
-	// 4. On valide
+	await page.locator('#email').fill('admin@admin.admin')
+	await page.locator('#password').fill('admin')
+
 	await page.locator('button[type="submit"]').click()
 
-	// 5. On attend la BONNE redirection (vers l'accueil)
 	await page.waitForURL('http://localhost:5173/', { timeout: 15000 })
 
-	// 6. On sauvegarde l'état
 	await page.context().storageState({ path: 'tests/.auth/user.json' })
 })
