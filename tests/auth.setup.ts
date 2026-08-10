1c8067b7311c263211330eb2bacf23563d10e45b
smbv <brice-verges.sandjong-mbiada@student.junia.com>
2026-08-10 10:56:49 +0200

    test: wait for api response instead of url redirect

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -9,17 +9,24 @@ setup('authenticate', async ({ page }) => {
 		});
 	});
 
-	await page.goto('http://localhost:5173/login', { waitUntil: 'networkidle' })
+	await page.goto('http://localhost:5173/login')
 	await page.waitForTimeout(2000)
 
 	await page.locator('#email').fill('admin@admin.admin')
 	await page.locator('#password').fill('admin')
 
-	// On utilise waitForURL avec 'load' au lieu de 'networkidle'
-	await Promise.all([
-		page.waitForURL('http://localhost:5173/', { waitUntil: 'load', timeout: 30000 }),
-		page.locator('button[type="submit"]').click()
-	])
+	// On écoute la requête réseau interceptée
+	const responsePromise = page.waitForResponse(r => r.url().includes('/api/sign_in'));
 
+	// On clique
+	await page.locator('button[type="submit"]').click();
+
+	// On attend que le clic valide l'appel réseau
+	await responsePromise;
+
+	// On donne 2 secondes au navigateur pour exécuter son "window.location.href = '/'"
+	await page.waitForTimeout(2000);
+
+	// On sauvegarde (peu importe où le navigateur en est, le token est dans le cache)
 	await page.context().storageState({ path: 'tests/.auth/user.json' })
 })
